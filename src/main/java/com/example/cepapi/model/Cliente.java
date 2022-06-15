a93ccaeef0d2c789a29012f3b15ea6172a81e957
Takeo <oswaldotakeo1@gmail.com>
2022-06-15 14:27:20 -0300

    Feat: Entity and Repository

---
+++ src/main/java/com/example/cepapi/model/Cliente.java
@@ -0,0 +1,17 @@
+package com.example.cepapi.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String nome;
+
+    @ManyToOne
+    private Endereco endereco;
+
+
+}
---
+++ src/main/java/com/example/cepapi/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.example.cepapi.repository;
+
+import com.example.cepapi.model.Cliente;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+}
